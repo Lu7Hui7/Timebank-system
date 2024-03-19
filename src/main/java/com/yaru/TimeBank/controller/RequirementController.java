e0c24dc1621723959ca8b7c30e5eddcfcb83e5a8
norp <1250453995@qq.com>
2024-03-19 17:59:36 +0800

    老人发布需求

--- src/main/java/com/yaru/TimeBank/controller/RequirementController.java
+++ src/main/java/com/yaru/TimeBank/controller/RequirementController.java
@@ -5,15 +5,50 @@ import com.yaru.TimeBank.common.R;
 import com.yaru.TimeBank.entity.Requirement;
 import com.yaru.TimeBank.service.RequirementService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Slf4j
 @RestController
 @RequestMapping("/request")
 public class RequirementController {
     private RequirementService requirementService;
+
+    /**
+     * 创建需求
+     *
+     * @param elderId      老人ID
+     * @param requirement  需求对象
+     * @return             创建需求结果
+     */
+    @PostMapping("/{elderId}/requirements")
+    public R<String> createRequirement(
+            @PathVariable Long elderId,
+            @RequestBody  Requirement requirement) {
+
+        // 从 RequirementRequest 对象中获取参数
+        String serviceName = requirement.getServiceName();
+        String serviceContent = requirement.getServiceContent();
+        LocalDate createTime = requirement.getCreateTime();
+        Long durationHours = requirement.getDurationHours();
+
+        // 创建需求对象
+        Requirement re = new Requirement();
+        re.setServiceName(serviceName);
+        re.setServiceContent(serviceContent);
+        re.setCreateTime(createTime);
+        re.setDurationHours(durationHours);
+
+        // 设置老人ID
+        re.setElderId(elderId);
+
+        // 保存需求到数据库
+        requirementService.save(re);
+
+        return R.success("成功添加需求!");
+    }
+
+
 
 }
